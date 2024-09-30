72e791bd61a728ed74afec0143b2ac4e861bb43c
Matthew DuBois <147000029+duboimd@users.noreply.github.com>
2024-09-30 12:59:46 -0600

    Create if-elif.py

---
+++ if-elif.py
@@ -0,0 +1,64 @@
+# CONDITIONAL IF-STATEMENTS
+
+print("Number 1:")
+# Q1: if num1 is greater than num2, print "Num is small!" 
+# Do not touch this code: 
+num1 = 3
+num2 = 6
+
+# Write your code here:
+
+print("Number 2:")
+# Q2: if age >= 18 print “You can vote!”
+# Do not touch this code: 
+age = 20
+
+# Write your code here:
+
+print("Number 3:")
+# Q3: if snow_outside is equal to True, print "Lets go sledding!" 
+
+# Do not touch this code: 
+snow_outside = True
+
+# Write your code here:
+
+print("Number 4:")
+# Q4: if rain_outside is True, print "Let’s go jump in puddles!" 
+
+# Do not touch this code: 
+rain_outside = True
+
+# Write your code here:
+
+print("Number 5:")
+# Q5: if sunny_outside is True, print "Lets go swimming!" 
+
+# Do not touch this code: 
+sunny_outside = False
+
+# Write your code here:
+
+print("Number 6:")
+# Q6: if homework_done is True, print "I finished my homework!”. Else print “I will go do my homework now!”
+
+# Do not touch this code: 
+homework_done = False
+
+# Write your code here:
+
+print("Number 7:")
+# Q7: if snow_day is True, print "No school today!" else print "We have school today" 
+
+# Do not touch this code: 
+snow_day = True
+
+# Write your code here:
+
+print("Number 8:")
+# Q8: if tired is True, print "I will go to sleep!" else print "I will go to sleep later" 
+
+# Do not touch this code: 
+tired = True
+
+# Write your code here:
